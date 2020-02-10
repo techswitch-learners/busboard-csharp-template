7de7e8be78545b1938472552a0f363c346b23ae0
Kanwaljit Dilawari <Kanwaljit.Dilawari@softwire.com>
2020-02-10 12:16:27 +0000

    connected to tfl api using RestSharp

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard
 {
@@ -7,6 +9,14 @@ namespace BusBoard
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            var client = new RestClient("https://api.tfl.gov.uk/");
+            client.Authenticator = new HttpBasicAuthenticator("f8163132", "fc375759d5162e056d8437f676e78aef");
+
+            var request = new RestRequest("StopPoint/490008660N", DataFormat.Json);
+
+            var response = client.Get(request);
+            Console.WriteLine(response);
         }
     }
 }
