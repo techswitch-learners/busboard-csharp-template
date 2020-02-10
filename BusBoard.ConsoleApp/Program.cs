abeb7fd1a018faf21cfb1684bdc13e8468cf8ba1
Kanwaljit Dilawari <Kanwaljit.Dilawari@softwire.com>
2020-02-10 12:53:51 +0000

    add prompt and response to return data for next five buses

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -13,10 +13,24 @@ namespace BusBoard
             var client = new RestClient("https://api.tfl.gov.uk/");
             client.Authenticator = new HttpBasicAuthenticator("f8163132", "fc375759d5162e056d8437f676e78aef");
 
-            var request = new RestRequest("StopPoint/490008660N", DataFormat.Json);
+            //var request = new RestRequest("StopPoint/490008660N", DataFormat.Json);
 
-            var response = client.Get(request);
-            Console.WriteLine(response);
+            //var response = client.Get(request);
+            
+            Console.WriteLine("Type a bus stop code to see the next 5 buses at that stop");
+            var busStop = Console.ReadLine();
+            
+            var nextFiveBusesRequest = new RestRequest($"StopPoint/{busStop}", DataFormat.Json);
+            var nextFiveBusesResponse = client.Get(nextFiveBusesRequest);
+
+            var responseString = nextFiveBusesResponse.ToString();
+
+            foreach (var word in responseString)
+            {
+                Console.Write(word);
+            }
+
+            
         }
     }
 }
